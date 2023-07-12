d81de0e9716b836e6d19f185ef307b2de92820f3
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-07-12 18:42:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 //complete this code
-class Person {}
+class Person {
+	 constructor(name,age){
+		this.name=name;
+		this.age=age;
+       }
+	 get name() {
+        return this._name;
+      }
+     get age() {
+        return this._age;
+       }
+     set age(value) {
+        this._age = value;
+       }
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(this.name);
+     }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(this.name);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
